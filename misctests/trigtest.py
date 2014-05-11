5f578d3a11a60dafde6265daa77f071920bfecbf
ashwinashok9111993 <ashwinashok9111993@gmail.com>
2014-05-12 00:38:23 +0530

    Create trigtest.py

---
+++ misctests/trigtest.py
@@ -0,0 +1,47 @@
+__author__ = 'ashwin'
+"""
+simple test 2
+"""
+import pycuda.driver as drv
+import pycuda.tools
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+import numpy
+
+import matplotlib.pyplot as p
+
+mod = SourceModule("""
+__global__ void gpusin(float *dest, float *a)
+{
+  const int i = threadIdx.x+(blockIdx.x*(blockDim.x*blockDim.y));
+  dest[i] = sinf(a[i]) ;
+}
+
+__global__ void gpucos(float *dest, float *a)
+{
+  const int i = threadIdx.x+(blockIdx.x*(blockDim.x*blockDim.y));
+  dest[i] = cosf(a[i]) ;
+}
+
+
+""")
+
+gpusin = mod.get_function("gpusin")
+gpucos = mod.get_function("gpucos")
+
+a = (numpy.arange(400)).astype(numpy.float32)
+b = numpy.sin(a)
+c = numpy.cos(a)
+
+destsin = numpy.zeros_like(a)
+destcos = numpy.zeros_like(a)
+
+gpusin(drv.Out(destsin), drv.In(a), block=(40, 1, 1), grid=(10, 1, 1))
+gpucos(drv.Out(destcos), drv.In(a), block=(40, 1, 1), grid=(10, 1, 1))
+
+p.subplot(2,1,1)
+p.plot(destsin)
+p.subplot(2,1,2)
+p.plot(destcos)
+p.show()
