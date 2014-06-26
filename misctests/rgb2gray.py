e53773e14e600e310a1d857ddec63be8238af839
ashwin <ashwinashok9111993@gmail.com>
2014-06-26 12:54:20 +0530

    made a few enhancements

---
+++ misctests/rgb2gray.py
@@ -0,0 +1,13 @@
+__author__ = 'ashwin'
+
+#import pycuda.driver as drv
+#import pycuda.tools
+#import pycuda.autoinit
+import numpy
+#from pycuda.compiler import SourceModule
+import numpy as np
+import scipy.misc as scm
+
+lena=scm.imread('Lenna.png').astype(np.uint8)
+print lena.shape
+
