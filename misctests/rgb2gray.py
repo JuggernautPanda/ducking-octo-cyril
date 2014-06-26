05e9ecd970cf626194520ff755d73fe6b2e25b70
ashwin <ashwinashok9111993@gmail.com>
2014-06-26 12:57:51 +0530

    made a few enhancements

--- misctests/rgb2gray.py
+++ misctests/rgb2gray.py
@@ -1,12 +1,21 @@
 __author__ = 'ashwin'
 
-#import pycuda.driver as drv
-#import pycuda.tools
-#import pycuda.autoinit
+import pycuda.driver as drv
+import pycuda.tools
+import pycuda.autoinit
 import numpy
-#from pycuda.compiler import SourceModule
+from pycuda.compiler import SourceModule
 import numpy as np
 import scipy.misc as scm
+
+
+mod = SourceModule("""
+__global__ void multiply_them(float *dest, float *a, float *b)
+{
+  const int i = threadIdx.x+(blockIdx.x*(blockDim.x*blockDim.y));
+  dest[i] = a[i] * b[i];
+}
+""")
 
 lena=scm.imread('Lenna.png').astype(np.uint8)
 print lena.shape
